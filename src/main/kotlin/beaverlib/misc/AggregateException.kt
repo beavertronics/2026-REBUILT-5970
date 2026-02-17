1846a9841c21fc09eab7246de77355325882f6ad
Sketched Doughnut <noreply>
2026-02-16 17:03:10 -0800

    Deleting beaverlib to reinstall

--- src/main/kotlin/beaverlib/misc/AggregateException.kt
+++
@@ -1,7 +0,0 @@
-package frc.beaverlib.misc
-
-class AggregateException(message: String?, primary: Exception?, vararg others: Throwable) : Exception(message, primary) {
-    init {
-        others.forEach {err -> addSuppressed(err)}
-    }
-}
